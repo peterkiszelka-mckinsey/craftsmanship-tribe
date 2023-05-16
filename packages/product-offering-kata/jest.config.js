2cbd98e00636175bee06685abd130624f9ba3aee
Peter Kiszelka <peter_kiszelka@mckinsey.com>
2023-05-17 00:42:15 +0200

    Set unit testing up

---
+++ packages/product-offering-kata/jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testTimeout: 90000,
+  transform: {
+    '.(ts|tsx)$': 'ts-jest'
+  },
+  rootDir: 'src'
+};
